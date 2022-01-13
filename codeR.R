20890b6f48af335be946036ded7cb6632dc9723a
Louis BOILLY <louis.boilly@gmail.com>
2022-01-13 16:58:43 +0100

    Plan d'action opérationnel

--- codeR.R
+++ codeR.R
@@ -1,11 +1,15 @@
 library(ggplot2)
 library(FactoMineR)
-setwd("/Users/louisparis/Desktop/Ponts 2A/STNUM/Projet")
+
 red_wine <- read.csv("wineQualityReds.csv",header=TRUE,sep=",",dec=",")
 white_wine <- read.csv("wineQualityWhites.csv", header=TRUE, sep=",", dec=",")
 
 
+Xmax = max(red_wine$X)
+white_wine$X = white_wine$X + Xmax
 
+red_wine$color = "red"
+white_wine$color = "white"
 
-
+all_wine = rbind(red_wine, white_wine)
 
